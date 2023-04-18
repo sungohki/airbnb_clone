2c78d135c14ac2d75329ca0739a8e2e8a5250f33
Sungoh Kim <sungohki@c6r2s8.42seoul.kr>
2023-04-18 13:33:22 +0900

    new : image.js added

---
+++ js/image.js
@@ -0,0 +1,34 @@
+
+// Variables
+const	IMGS_SOURCE = "../images/";
+const	imgCard = [
+	"card1.jpg",
+	"card2.jpg",
+	"card3.jpg",
+	"card4.jpg"
+];
+const	imgChoo = [
+	"choo1.png",
+	"choo2.png",
+	"choo3.png",
+	"choo4.png"
+];
+const	imgHome = [
+	"home1.png",
+	"home2.png",
+	"home3.png",
+	"home4.png",
+	"home5.png",
+	"home6.png",
+	"home7.png",
+	"home8.png",
+]
+const	cardBoxBg = document.querySelector("div.cardBox");
+const	chooBoxBg = document.querySelector("div.chooBox");
+const	homeBoxBg = document.querySelector("div.homeBox");
+
+// Inits
+
+// Functions
+
+// Events
